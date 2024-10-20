caf3e303384f73197f655ea2169da8af78f13df3
siva <sivasatyan.b@gmail.com>
2024-10-20 12:24:10 +0100

    q3 init

---
+++ 2023/q3.py
@@ -0,0 +1,9 @@
+initialState = input().split(' ')
+initialState = [[int(digit) for digit in n] for n in initialState]
+
+# print(initialState)
+
+finalState = input().split(' ')
+finalState = [[int(digit) for digit in n] for n in finalState]
+
+# print(finalState)
