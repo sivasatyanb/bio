82b9d2e93b67f4d1f9539322ac15806b7cad8a8b
siva <sivasatyan.b@gmail.com>
2024-10-26 16:46:58 +0100

    restructure again

--- 2024 _old/2024_q1a.py
+++ 2024 _old/2024_q1a.py
@@ -1,24 +1,12 @@
-# part "../2024"a:
-
 n, index = input().split(' ')
-
 n = int(n)
 index = int(index)
-i = index
-
 string = ''
-string += str(n)
-i -= len(str(n))
 
-while True:
-    n += 1
+for i in range(index):
     string += str(n)
-    i -= len(str(n))
-
-    # print(string)
-    # print(i)
-
-    if i <= 0:
-        print(string)
-        print(int(string[index-1]))
+    n += 1
+    if len(string) > index:
         break
+
+print(string[index-1])
